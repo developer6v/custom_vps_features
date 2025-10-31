3b1cb2c2e0caf370a5987dcbc3d43c754c182e00
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-31 17:12:13 -0300

    first commit

--- hooks.php
+++ hooks.php
@@ -3,11 +3,17 @@
 require_once('src/Services/index.php');
 
 add_hook('ClientAreaFooterOutput', 1, function (array $vars) {
-    $varsJson = json_encode(
-        $vars,
-        JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
-        | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
-    );
 
-    return "<script>console.log($varsJson);</script>";
+    if ($vars["action"] == "productdetails") {
+        $varsJson = json_encode(
+            $vars,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+            | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+        );
+
+        return "<script>console.log($varsJson);</script>";
+    }
+
+    return "";
+
 });
