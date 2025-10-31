38e56f2b61b62fd58c03fcd2a22394b4370f790a
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-31 17:32:48 -0300

    first commit

--- hooks.php
+++ hooks.php
@@ -5,15 +5,8 @@ require_once('src/Services/index.php');
 add_hook('ClientAreaFooterOutput', 1, function (array $vars) {
 
     if ($vars["action"] == "productdetails") {
-    $serviceId = (int)($vars['serviceid'] ?? $vars['id'] ?? ($_GET['id'] ?? 0));
-
-    return "<script>console.log('serviceId (hook): {$serviceId}');</script>";
-        return "<script>
-        const url = new URL(window.location.href);
-        const id = url.searchParams.get('id');
-        console.log('id da URL:', id);
-        
-        console.log($varsJson);</script>";
+        $serviceId = (int)($vars['serviceid'] ?? $vars['id'] ?? ($_GET['id'] ?? 0));
+        return manage_status_vps($serviceId);
     }
 
     return "";
--- src/Services/WHMCS/manage_status_vps.php
+++ src/Services/WHMCS/manage_status_vps.php
@@ -1,8 +1,29 @@
 <?php
 
 
-function manage_status_vps() {
-    
+function manage_status_vps($serviceId) {
+    $params = [
+        'serviceid' => $serviceId,
+        'stats'     => true,    
+    ];
+
+    $result = localAPI('GetClientsProducts', $params);
+    $productname = $result["products"]["product"][0]["name"];
+    if (strpos($productname, 'VPS') !== false) {
+        $ip = $result["products"]["product"][0]["dedicatedip"];
+        if ($ip == "" || !$ip) {
+            return "
+                <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    var el = document.querySelector('.status');
+                    if (el) { el.textContent = 'Pendente'; }
+                });
+                </script>
+            ";
+        }
+    }
+
+    return "<script>console.log('não encontrou');</script>";
 }
 
 
