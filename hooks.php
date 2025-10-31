b9c1f1aaffbdae4b8cbd0e93414e238bfe734334
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-31 17:05:04 -0300

    first commit

--- hooks.php
+++ hooks.php
@@ -1,1 +1,13 @@
 <?php
+
+require_once('src/Services/index.php');
+
+add_hook('ClientAreaFooterOutput', 1, function (array $vars) {
+    $varsJson = json_encode(
+        $vars,
+        JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+        | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP
+    );
+
+    return "<script>console.log($varsJson);</script>";
+});
---
+++ src/Services/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once('WHMCS/manage_status_vps.php');
